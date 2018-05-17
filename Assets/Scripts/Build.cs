066cb2e770916429195a08d37f3204f43ecddde3
reygar91 <acc91@ukr.net>
2018-05-17 18:22:08 +0300

    17.05.18
    Deleted InteriorPreview; Room changed into BuildPreview; many methods of Build moved to BuildPreview.

--- Assets/Scripts/Build.cs
+++ Assets/Scripts/Build.cs
@@ -6,49 +6,28 @@ using UnityEngine.UI;
 public class Build : MonoBehaviour {
 
     public Toggle[] RoomOption, InteriorOption;
-    public GameObject[] RoomOriginals;
+    public BuildPreview[] RoomOriginals;
     /*Room Originals:
      * 0 - RoomSize 6
      * 1 - RoomSize 9
      * 2 - RoomSize 12
-     */    
-    public GameObject RoomContainer, InteriorPrewiewGO;
-    private GameObject RoomGO;
-    private InteriorPreview IntPreview;
-    private Room RoomScript;
+     */
+    public RectTransform SizeGroup;
+    private BuildPreview Preview;
+    private int InteriorIndex;
+    
 
-    private void Awake()
-    {
-        RoomGO = RoomOriginals[0];
-        IntPreview = InteriorPrewiewGO.GetComponent<InteriorPreview>();
-    }
 
     public void SelectRoom(int i)
     {
         if (RoomOption[i].isOn)
         {
-            RoomGO.SetActive(false);
-            RoomGO = RoomOriginals[i];
-            RoomGO.SetActive(true);
-            RoomScript = RoomGO.GetComponent<Room>();
+            Preview = RoomOriginals[i];
+            Preview.Interior = Preview.InteriorOption[InteriorIndex];
+            Preview.gameObject.SetActive(true);
             RoomOption[i].isOn = false; // Disabling Toggle, so dont have to click twice when building similar constructions
-        }
-    }
-
-    private void SelectBuildPosition()
-    {
-        Vector3 newPosition = UI_helper.MoveWithMouse(RoomGO, 3, new Vector3(0, -0.1f, 0.9f));
-        if (Input.GetMouseButtonDown(0) && !UI_helper.isPointerOverUI())
-        {
-            if (RoomScript.allowedToBuild == true)
-            {
-                GameObject newRoom = Instantiate(RoomGO, RoomContainer.transform);
-                newRoom.transform.position = newPosition;
-                RoomGO.SetActive(false);
-            } else
-            {
-                Debug.Log("Not allowed to build here");
-            }            
+            InteriorOption[InteriorIndex].isOn = false;
+            SizeGroup.gameObject.SetActive(false);
         }
     }
 
@@ -56,28 +35,17 @@ public class Build : MonoBehaviour {
     {
         if (InteriorOption[i].isOn)
         {
-            InteriorPrewiewGO.SetActive(true);
-            IntPreview.InteriorIndex = i;
-            InteriorOption[i].isOn = false; // Disabling Toggle, so dont have to click twice when building similar constructions
-        }
-    }
-
-    // Update is called once per frame
-    void LateUpdate () {        
-        if (RoomGO.activeSelf == true)
-        {
-            SelectBuildPosition();
-            if (Input.GetMouseButtonDown(1))
-            {
-                RoomGO.SetActive(false);
-            }
+            InteriorIndex = i;
+            SizeGroup.gameObject.SetActive(true);
         }
     }
 
     private void OnDisable()
     {
-        RoomGO.SetActive(false);
-        InteriorPrewiewGO.SetActive(false);
+        if (Preview)
+        {
+            Preview.gameObject.SetActive(false);
+        }
     }
 
 }
---
+++ Assets/Scripts/BuildPreview.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildPreview : MonoBehaviour {
+
+    //public GameObject Doors;
+    public GameObject[] InteriorOption;
+    /*
+     * 0 - Bar;
+    */
+    public GameObject Interior, InteriorContainer, RoomContainer;
+    private bool allowedToBuild = true;
+
+    private void OnEnable()
+    {
+        Interior.transform.SetParent(transform);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Room")
+        {
+            allowedToBuild = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Room")
+        {
+            allowedToBuild = true; //Debug.Log("Colliding with other Room");
+        }
+    }
+
+    private void SelectBuildPosition()
+    {
+        Vector3 newPosition = UI_helper.MoveWithMouse(gameObject, 3, new Vector3(0, -0.1f, 0.9f));
+        if (Input.GetMouseButtonDown(0) && !UI_helper.isPointerOverUI())
+        {
+            if (allowedToBuild == true)
+            {
+                GameObject newRoom = Instantiate(gameObject, RoomContainer.transform);
+                newRoom.transform.position = newPosition;
+                Destroy(newRoom.GetComponent<BuildPreview>());
+                Interior.transform.SetParent(InteriorContainer.transform);
+                newRoom.name = Interior.name;
+                gameObject.SetActive(false);
+            }
+            else
+            {
+                Debug.Log("Not allowed to build here");
+            }
+        }
+    }
+
+    void LateUpdate()
+    {
+        SelectBuildPosition();
+        if (Input.GetMouseButtonDown(1))
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+}
